9483436ef3fe2a0f485834380e1c07676d14f2c3
Your Name <you@example.com>
2021-11-29 00:07:10 +0500

    edit

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -18,7 +18,7 @@ router.route('/product/:id').get(getSingleProduct);
 router.route('/admin/product/new').post(isAuthenticatedUser, newProduct);
 
 router.route('/admin/product/:id')
-            .put( updateProduct)
+            .put(isAuthenticatedUser, updateProduct)
             .delete(isAuthenticatedUser, deleteProduct);
 
 module.exports = router;
