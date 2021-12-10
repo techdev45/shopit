301c7e1178bf367c9607efdd3432b1bcb5f5006c
Umair Khalid <meiri4455@gmail.com>
2021-12-10 23:03:01 +0500

    by meiri

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -25,23 +25,23 @@ import {
         }
  }
 
- export const getProducts = () => async (dispatch) => {
-    try{
-        dispatch({ type: ALL_PRODUCTS_REQUEST})
+//  export const getProducts = () => async (dispatch) => {
+//     try{
+//         dispatch({ type: ALL_PRODUCTS_REQUEST})
 
-        const { data } = await axios.get('/api/v1/products')
+//         const { data } = await axios.get('/api/v1/products')
 
-        dispatch({ type: ALL_PRODUCTS_SUCCESS,
-        payload: data
-    })
+//         dispatch({ type: ALL_PRODUCTS_SUCCESS,
+//         payload: data
+//     })
 
-    } catch (error) {
-        dispatch({
-            type:ALL_PRODUCTS_FAIL, 
-            payload: error.response.data.message
-        })
-    }
-}
+//     } catch (error) {
+//         dispatch({
+//             type:ALL_PRODUCTS_FAIL, 
+//             payload: error.response.data.message
+//         })
+//     }
+// }
 
  // clear Errors
  export const clearErrors = () => async (dispatch) => {
