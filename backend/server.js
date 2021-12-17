debdc8f08d5fbede69f4c47b1170e49bf8155d26
Umair Khalid <meiri4455@gmail.com>
2021-12-17 15:00:28 +0500

    by meiri

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,6 @@ const connectDatabase = require('./config/database')
 const dotenv = require('dotenv');
 const cloudinary = require('cloudinary')
 
-
-
-
-
-
-
-
 //Handle Uncaught exceptions 
 process.on('uncaughtException', err => {
   console.log(`ERROR: ${err.stack}`);
