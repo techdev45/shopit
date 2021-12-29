0010c3412632ccbd23c925615ef0939e4c2268a7
aunjaffery <aunjaffery123@gmail.com>
2021-12-29 15:11:14 +0500

    itsRunning

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,33 +1,33 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
+const {
+  getProducts,
+  newProduct,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
+} = require("../controllers/productController");
 
-const { getProducts,
-    newProduct,
-    getSingleProduct,
-    updateProduct,
-    deleteProduct,
-    createProductReview,
-    getProductReviews ,
-    deleteReview 
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
-} = require('../controllers/productController')
+router.route("/products").get(getProducts);
+router.route("/product/:id").get(getSingleProduct);
 
-const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+router
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
 
-router.route('/products').get(getProducts);
-router.route('/product/:id').get(getSingleProduct);
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
-router.route('/admin/product/new').post(isAuthenticatedUser,authorizeRoles('admin'), newProduct);
-
-router.route('/admin/product/:id')
-            .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
-            .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
-
-
-router.route('/review').put(isAuthenticatedUser, createProductReview )
-router.route('/reviews').get(isAuthenticatedUser, getProductReviews )
-router.route('/reviews').delete(isAuthenticatedUser, deleteReview )
-
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+router.route("/reviews").get(isAuthenticatedUser, getProductReviews);
+router.route("/reviews").delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
