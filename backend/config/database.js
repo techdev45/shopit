67e0e782ddc681288aaa6ae95f26fb6d9d3acbaa
Umair Khalid <meiri4455@gmail.com>
2022-01-02 00:09:39 +0500

    initial Commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config({ path: "backend/config/config.env" });
+// const dotenv = require("dotenv");
+// dotenv.config({ path: "backend/config/config.env" });
 
 const connectDatabase = () => {
-  mongoose.connect(process.env.DB_URI, {}).then((con) => {
+  mongoose.connect(process.env.DB_LOCAL_URI, {}).then((con) => {
     console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
   });
 };
