6a2e1225c1b9996100160fc8f40aa1618115786e
Umair Khalid <meiri4455@gmail.com>
2022-01-01 22:03:16 +0500

    initial Commit

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use("/api/v1", products);
 app.use("/api/v1", auth);
 app.use("/api/v1", order);
 
-if (process.env.NODE_ENV === "PRODUCTION") {
+if (process.env.NODE_ENV !== "PRODUCTION") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
