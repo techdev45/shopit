4b941f6fabfb55cf9cfc5a7ea601a4cf68ef1580
Umair Khalid <meiri4455@gmail.com>
2022-01-02 16:47:27 +0500

    make it better

--- backend/app.js
+++ backend/app.js
@@ -29,10 +29,10 @@ app.use("/api/v1", auth);
 app.use("/api/v1", order);
 
 if (process.env.NODE_ENV === "PRODUCTION") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
