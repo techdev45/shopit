028444f300b5fb5847bb2f1b208c63c005ec2d09
Umair Khalid <meiri4455@gmail.com>
2022-01-02 15:26:55 +0500

    initial Commit

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
-// const path = require("path");
+const path = require("path");
 
 const errorMiddleware = require("./middlewares/errors");
 
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// dotenv.config({ path: "backend/config/config.env" });
+const dotenv = require("dotenv");
+dotenv.config({ path: "backend/config/config.env" });
 
 const connectDatabase = () => {
   mongoose.connect(process.env.DB_URI, {}).then((con) => {
